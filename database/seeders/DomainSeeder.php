87110349a9799bd942d981b8d03aae15d813c5c6
Hubolux Technologies <pellyjosh.ng@gmail.com>
2025-07-10 10:17:35 +0100

    tenant seeders

--- database/seeders/DomainSeeder.php
+++ database/seeders/DomainSeeder.php
@@ -16,7 +16,9 @@ class DomainSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         DB::table('domains')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         $tenants = Tenant::all();
 
         foreach ($tenants as $tenant) {
--- database/seeders/TenantSeeder.php
+++ database/seeders/TenantSeeder.php
@@ -15,7 +15,9 @@ class TenantSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         DB::table('tenants')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         Tenant::create(['id' => 'estate1']);
         Tenant::create(['id' => 'estate2']);
     }
